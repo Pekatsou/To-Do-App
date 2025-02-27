9cb4c63c5f2ce124c7ff423db114bce4259da73e
Pekatsou <ryanerickfaustino@gmail.com>
2025-02-27 13:48:41 +0800

    vite reupload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/To-Do-App/', // Set this to your repository name
+  base: '/To-Do-App', // Set this to your repository name
 });
